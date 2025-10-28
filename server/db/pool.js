22688d86e74b653bcf221e43ecc6bec4d6fcd465
d0vid9 <64305701+baddav@users.noreply.github.com>
2025-10-28 20:17:00 +0100

    pool.js kommentiert

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,6 +1,16 @@
+/**
+ * Modul zum Erstellen eines MySQL-Datenbank-Pools.
+ */
 const mysql = require('mysql2');
+
+/**
+ * Lädt Umgebungsvariablen aus der .env-Datei.
+ */
 require('dotenv').config();
 
+/**
+ * Datenbank-Pool für die Verbindung zur Datenbank.
+ */
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
